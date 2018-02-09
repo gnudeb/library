97b697e00eb7697a25a3a4caec4e3596eb8258a4
gnudeb <gnudeb0@gmail.com>
2018-02-09 19:32:00 +0200

    Route search

--- app/forms.py
+++ app/forms.py
@@ -2,5 +2,5 @@ from wtforms import Form, StringField, validators
 
 
 class BookSearchForm(Form):
-    query = StringField('query', [validators.Length(min=3, max=64)])
-    email = StringField('email', [validators.email(message="Invalid email")])
+    query = StringField('Search query', [validators.Length(min=3, max=64)])
+    email = StringField('Email', [validators.email(message="Invalid email")])
--- app/views.py
+++ app/views.py
@@ -1,14 +1,27 @@
+from flask import request, redirect
 from flask.views import View, MethodView
 from flask.templating import render_template
 from .forms import BookSearchForm
 
+
 class IndexView(View):
+
     def dispatch_request(self):
-        search_form = BookSearchForm()
-        return render_template('search.html', search_form=search_form)
+        return redirect('/search/')
+
 
 class SearchView(MethodView):
 
-    def get(self):
-        search_form = BookSearchForm()
+    def get(self, search_form=None):
+        search_form = search_form or BookSearchForm()
         return render_template('search.html', search_form=search_form)
+
+    def post(self):
+        search_form = BookSearchForm(request.form)
+        if search_form.validate():
+            #TODO: Dispatch search
+            return "Searching for {}, results will be sent at {}".format(
+                search_form.query.data,
+                search_form.email.data)
+        else:
+            return self.get(search_form)
