aca6553ebe72735960ed20253ded418da4288049
gnudeb <gnudeb0@gmail.com>
2018-02-09 22:28:25 +0200

    Set up PostgreSQL support

--- app/config.py
+++ app/config.py
@@ -1,14 +1,21 @@
 import json
 import os.path
 
-SQLALCHEMY_DATABASE_URI = "postgresql://localhost/library"
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-MAIL_SERVER = 'smtp.sendgrid.net'
-MAIL_DEFAULT_SENDER = 'library'
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 SECRETS_FILEPATH = os.path.join(BASE_DIR, 'secrets.json')
 
 with open(SECRETS_FILEPATH, 'r') as secrets_file:
     secrets = json.load(secrets_file)
+
+PG_USERNAME = secrets['PG_USERNAME']
+PG_PASSWORD = secrets['PG_PASSWORD']
+SQLALCHEMY_DATABASE_URI = "postgresql://{username}:{password}@localhost/library".format(
+    username=PG_USERNAME,
+    password=PG_PASSWORD,
+)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+MAIL_SERVER = 'smtp.sendgrid.net'
+MAIL_DEFAULT_SENDER = 'library'
 MAIL_USERNAME = secrets['MAIL_USERNAME']
 MAIL_PASSWORD = secrets['MAIL_PASSWORD']
